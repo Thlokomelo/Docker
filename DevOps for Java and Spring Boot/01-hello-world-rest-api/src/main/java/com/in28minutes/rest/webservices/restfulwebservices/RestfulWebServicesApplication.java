7c0be4f6dc76f8c71ab4f0739742c385edc41f4a
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-11-21 10:29:41 +0200

    Updated app

---
+++ DevOps for Java and Spring Boot/01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication                            //spring context which launches the controller
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+}
